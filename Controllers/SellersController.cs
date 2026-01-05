6ee7e1985edb98042d338fa53e4d7722f8f42d4a
Nathã Targino <148729150+nathatargino@users.noreply.github.com>
2026-01-05 17:57:11 -0300

    Sellers Controller

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuraCommerce.Data;
+using AuraCommerce.Models;
+using System.Linq;
+
+namespace AuraCommerce.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly AuraCommerceContext _context;
+
+        public SellersController(AuraCommerceContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Busca a lista de vendedores no banco de dados
+            var list = _context.Seller.ToList();
+            return View(list);
+        }
+    }
+}
