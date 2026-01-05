bc974b9785ccab716453357ccd85f4643def8801
Nathã Targino <148729150+nathatargino@users.noreply.github.com>
2026-01-05 16:31:30 -0300

    Segunda migração e criado algumas entidades

--- Data/AuraCommerceContext.cs
+++ Data/AuraCommerceContext.cs
@@ -14,6 +14,8 @@ namespace AuraCommerce.Data
         {
         }
 
-        public DbSet<AuraCommerce.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
