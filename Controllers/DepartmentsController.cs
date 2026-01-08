1e727a136c8429cfbfc80c98da6c8f53383da710
Nathã Targino <148729150+nathatargino@users.noreply.github.com>
2026-01-08 11:00:54 -0300

    Realizado ajustes no DepartmentController para tratamento de exceção da exclusão de departamentos

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -1,41 +1,95 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuraCommerce.Models;
 using AuraCommerce.Services;
-using AuraCommerce.Models;
+using AuraCommerce.Services.Exceptions;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace AuraCommerce.Controllers
 {
     public class DepartmentsController : Controller
     {
-        
         private readonly DepartmentService _departmentService;
 
-        
         public DepartmentsController(DepartmentService departmentService)
         {
             _departmentService = departmentService;
         }
 
-
         public async Task<IActionResult> Index()
         {
-            var list = await _departmentService.FindAllAsync(); 
+            var list = await _departmentService.FindAllAsync();
             return View(list);
         }
 
-        // Abrir formulário (GET)
         public IActionResult Create()
         {
             return View();
         }
 
-        // Salva formulário (POST)
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Department department)
         {
-            await _departmentService.InsertAsync(department); 
+            await _departmentService.InsertAsync(department);
             return RedirectToAction(nameof(Index));
         }
+
+        // GET: 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = await _departmentService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = await _departmentService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // 2. POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _departmentService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                var errorViewModel = new ErrorViewModel
+                {
+                    Message = e.Message,
+                    RequestId = System.Diagnostics.Activity.Current?.Id
+                                ?? HttpContext.TraceIdentifier
+                };
+
+                return View("Error", errorViewModel);
+            }
+        }
+
     }
 }
--- Services/DepartmentService.cs
+++ Services/DepartmentService.cs
@@ -1,5 +1,6 @@
 ﻿using AuraCommerce.Data;
 using AuraCommerce.Models;
+using AuraCommerce.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,22 +17,34 @@ namespace AuraCommerce.Services
             _context = context;
         }
 
-        // Buscar todos os departamentos ordenados por nome
         public async Task<List<Department>> FindAllAsync()
         {
             return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
 
-        // Salva no banco
-        public async Task Insert(Department obj)
+        public async Task InsertAsync(Department obj)
         {
             _context.Add(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        internal async Task InsertAsync(Department department)
+        public async Task<Department> FindByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Department.FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var obj = await _context.Department.FindAsync(id);
+                _context.Department.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Não é possível deletar este departamento porque ele possui vendedores associados.");
+            }
         }
     }
 }
